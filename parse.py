f04b439376723c13219f4946d9a0010ff2cccc80
alikhamesy <alikhamesy0@gmail.com>
2020-12-10 22:08:26 -0800

    wip parsing data

--- parse.py
+++ parse.py
@@ -1,10 +1,25 @@
 import sys
 import json
 import pandas as pd
+import numpy as np
+from releases import releases
 
 in_file = pd.read_json(sys.argv[1], orient='index')
-# out_file = open(sys.argv[2], 'w')
 
-# champions = json.loads(in_file)
+def parse(row):
+  return pd.Series({
+    'champion': row['id'],
+    'date': releases[row['id']],
+    'passive': row['passive']['description'],
+    'q': row['spells'][0]['description'],
+    'w': row['spells'][1]['description'],
+    'e': row['spells'][2]['description'],
+    'r': row['spells'][3]['description'],
+  })
 
-print(pd.json_normalize(in_file['data']).columns)
+champions = pd.json_normalize(in_file['data'], max_level=0).apply(parse, axis=1)
+
+champions['q'] = champions['q'].str.split()
+print(champions['q'])
+# print(champions.explode('q').groupby(by='champion').count())
+
